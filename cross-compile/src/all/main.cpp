8baf227a6a41bdcb5e6c3e73777313e752147c4d
Greg Hall <greghall@bantling.me>
2021-11-02 07:59:49 -0300

    Build windows static binary

--- cross-compile/src/all/main.cpp
+++ cross-compile/src/all/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello" << std::endl;
+	std::cout << "Hello";
+#ifdef _WIN32
+	std::cout << " Windows";
+#else
+	std::cout << " Linux";
+#endif
+#ifdef DEBUG
+	std::cout << " Debug";
+#else
+	std::cout << " Release";
+#endif
+	std::cout << std::endl;
 }
