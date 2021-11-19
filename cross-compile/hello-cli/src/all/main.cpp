a68c6b9995ee46a82de7ee38e7740334acf15768
Greg Hall <greghall@bantling.me>
2021-11-19 08:24:26 -0400

    All Makefiles are generic

--- cross-compile/hello-cli/src/all/main.cpp
+++ cross-compile/hello-cli/src/all/main.cpp
@@ -2,15 +2,23 @@
 
 int main() {
 	std::cout << "Hello";
-#ifdef WINDOWS
-	std::cout << " Windows";
-#else
+#if defined LINUX
 	std::cout << " Linux";
+#elif defined WINDOWS
+	std::cout << " Windows";
+#elif defined OSX
+	std::cout << " OSX";
+#elif defined FREEBSD
+	std::cout << " FreeBSD";
+#else
+#error Unsupported platform
 #endif
+
 #ifdef DEBUG
 	std::cout << " Debug";
 #else
 	std::cout << " Release";
 #endif
+
 	std::cout << std::endl;
 }
