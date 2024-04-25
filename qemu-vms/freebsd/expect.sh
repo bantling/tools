2c2f5a225389e49f3a8fcdf8da32d6d0b3404f2a
Greg Hall <greghall@bantling.me>
2024-04-25 17:27:27 -0300

    Changes

--- qemu-vms/freebsd/expect.sh
+++ qemu-vms/freebsd/expect.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/expect -f
 
-# Fire up telnet
-spawn telnet -e '`' localhost 2323
+set accel "[lindex $argv 0]"
+set memstick "[lindex $argv 1]"
+set image "[lindex $argv 2]"
+
+puts "Accel    = $accel"
+puts "Memstick = $memstick"
+puts "Image    = $image"
+
+# Fire up qemu
+spawn qemu-system-x86_64 \
+  -accel "$accel" \
+  -boot c \
+  -cpu qemu64 \
+  -m 1024 \
+  -drive "file=$memstick,format=raw,if=virtio" \
+  -drive "file=$image,format=raw,if=virtio" \
+  -nographic
 
 match_max 100000
-expect "*escape character is '`'.*"
-send -- "`"
-sleep 1
-send -- "quit\r"
+expect "*Autoboot in*"
+send -- "\r"
+interact +++ return
+send -- "root\r"
+expect "*root@:~ #"
+send -- "poweroff\r"
 expect eof
