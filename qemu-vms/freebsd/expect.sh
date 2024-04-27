f006ceab60385e5fe61a690857ea9b60215c9d12
Greg Hall <greghall@bantling.me>
2024-04-26 21:11:02 -0300

    Changes

--- qemu-vms/freebsd/expect.sh
+++ qemu-vms/freebsd/expect.sh
@@ -7,6 +7,9 @@ set image "[lindex $argv 2]"
 puts "Accel    = $accel"
 puts "Memstick = $memstick"
 puts "Image    = $image"
+puts "Hit \` when the login: prompt appears"
+puts "Hit return to continue or Ctrl-C to stop"
+expect_user -re "(.*)\n"
 
 # Fire up qemu
 spawn qemu-system-x86_64 \
@@ -19,9 +22,10 @@ spawn qemu-system-x86_64 \
   -nographic
 
 match_max 100000
+# Hit return at boot menu for default option
 expect "*Autoboot in*"
 send -- "\r"
-interact +++ return
+interact ` return
 send -- "root\r"
 expect "*root@:~ #"
 send -- "poweroff\r"
