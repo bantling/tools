e3e8aa02d12d12ec34f72a1ca468921673499809
Greg Hall <greghall@bantling.me>
2024-05-10 09:23:09 -0300

    Changes

--- qemu-vms/freebsd/bootstrap.sh
+++ qemu-vms/freebsd/bootstrap.sh
@@ -1,13 +1,58 @@
 #!/bin/sh
 set -eu
 
+# Do we need to reboot?
+reboot=0
+
+# Check if /boot/loader.conf contains lines for disk identification settings (not present by default)
+grep -q 'kern.geom.label' /boot/loader.conf || {
+  # We need to reboot for these settings to take effect
+  reboot=1
+
+  # Remount root fs read/write
+  mount -u -w /
+
+  # Inform user
+  echo 'Configuring gptid disk identification'
+
+  # Append lines to the end
+  {
+    echo 'kern.geom.label.disk_ident.enable="0"'
+    echo 'kern.geom.label.gpt.enable="0"'
+    echo 'kern.geom.label.gptid.enable="1"'
+  } >> /boot/loader.conf
+}
+
+# Check if /etc/rc.local runs this script automatically at boot
+grep -q bootstrap.sh /etc/rc.local || {
+  # Rebooting allows user to manually test this script launches automatically
+  reboot=1
+
+  # Remount root fs read/write
+  mount -u -w /
+
+  # Inform user
+  echo 'Ensuring bootstrap.sh runs automatically at boot'
+
+  # Modify /etc/rc.local
+  sed -rie '1s,(.*),\1\n\nsh /bootstrap.sh\nexit,' /etc/rc.local
+}
+
+# Reboot if necessary
+[ "$reboot" -eq 0 ] || {
+  # Inform user
+  echo 'Rebooting'
+
+  reboot
+}
+
 # Download setup.sh script, in case it has changed
-echo 'Downloading latest setup.sh from github'
-curl -o /root/setup.sh "https://github.com/bantling/tools/blob/master/qemu-vms/freebsd/setup.sh"
+echo 'Downloading latest setup.sh via ftp'
+(printf "GET /bantling/tools/master/qemu-vms/freebsd/setup.sh HTTP/1.0\r\nHOST: raw.githubusercontent.com\r\n\r\n"; sleep 3) | openssl s_client -connect raw.githubusercontent.com:443 -quiet | sed -n '/#!/,$p' > setup.sh
 
 # Execute setup script
 echo 'Executing setup.sh'
-sh /root/setup.sh
+sh /setup.sh
 
 # Shut down
 poweroff
--- qemu-vms/freebsd/setup.sh
+++ qemu-vms/freebsd/setup.sh
@@ -1,47 +1,5 @@
 #!/bin/sh
 
-# Do we need to reboot?
-reboot=0
-
-# Remount root fs read/write
-mount -u -w /
-
-# Check if /boot/loader.conf contains lines for disk identification settings (not present by default)
-grep -q 'kern.geom.label' /boot/loader.conf || {
-  # We need to reboot for these settings to take effect
-  reboot=1
-
-  # Inform user
-  echo 'Configuring gptid disk identification'
-
-  # Append lines to the end
-  {
-    echo 'kern.geom.label.disk_ident.enable="0"'
-    echo 'kern.geom.label.gpt.enable="0"'
-    echo 'kern.geom.label.gptid.enable="1"'
-  } >> /boot/loader.conf
-}
-
-# Check if /etc/rc.local runs this script automatically at boot
-grep -q bootstrap.sh /etc/rc.local || {
-  # Rebooting allows user to manually test this script launches automatically
-  reboot=1
-
-  # Inform user
-  echo 'Ensuring bootstrap.sh runs automatically at boot'
-
-  # Modify /etc/rc.local
-  sed -rie '1s,(.*),\1\n\nsh /root/bootscript.sh\nexit,' /etc/rc.local
-}
-
-# Reboot if necessary
-[ "$reboot" -eq 0 ] || {
-  # Inform user
-  echo 'Rebooting'
-
-  reboot
-}
-
 ## Destroy any existing gpt partition on second drive
 echo 'Destroying gpt partition table'
 gpart destroy -F vtbd1
