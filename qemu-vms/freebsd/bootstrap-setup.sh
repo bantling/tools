09a022ec3dcf86b7a7a6d5543eb2b2dcb9e314a0
Greg Hall <greghall@bantling.me>
2024-05-22 09:08:28 -0300

    Changes

--- qemu-vms/freebsd/bootstrap-setup.sh
+++ qemu-vms/freebsd/bootstrap-setup.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 set -eu
 
+# Check if the /boot/loader.conf file has an entry for setting the autoboot delay to 1
+grep -q autoboot_delay /boot/loader.conf || {
+  # Inform user
+  echo 'Setting autoboot delay to 1 second'
+  sleep 1
+
+  # Modify boot loader
+  echo 'autoboot_delay="1"' >> /boot/loader.conf
+
+  # Don't bother rebooting, setup.sh will do that
+}
+
 # Check if /etc/rc.local runs this script automatically at boot
 grep -q bootstrap.sh /etc/rc.local || {
   # Inform user
   echo 'Ensuring bootstrap.sh runs automatically at boot'
+  sleep 1
 
   # Modify /etc/rc.local
   sed -rie '1s,(.*),\1\n\nsh /bootstrap.sh\nexit,' /etc/rc.local
@@ -14,20 +27,29 @@ grep -q bootstrap.sh /etc/rc.local || {
 
 # Download setup.sh script, in case it has changed
 echo 'Downloading latest setup.sh'
+sleep 1
 echo "get setup.sh" | tftp 10.0.2.2
 
 # Execute setup script
-echo 'Executing setup.sh'
-sh /setup.sh vtbd1 -n
+echo 'Executing setup.sh (no swap, poolname = zinstall, autologin)'
+sleep 1
+sh /setup.sh vtbd1 -n -p zinstall -a
 
-# If this is first time running setup.sh, it reboots, and this code does not execute until next boot
+# Copy setup script and base and kernel sets
 echo 'Copying setup.sh and base and kernel sets to new zfs filesystem'
+sleep 1
 cp /setup.sh /tmp/zfs
 chmod +x /tmp/zfs/setup.sh
 
 mkdir -p /tmp/zfs/usr/freebsd-dist
 cp /usr/freebsd-dist/base.txz /tmp/zfs/usr/freebsd-dist
 cp /usr/freebsd-dist/kernel.txz /tmp/zfs/usr/freebsd-dist
+
+# Auto login as root
+echo 'Autologin as root'
+sleep 1
+echo '/bin/sh' >> /tmp/zfs/etc/rc.local
+chmod +x /tmp/zfs/etc/rc.local
 
 # Shut down
 echo 'Shutting down'
--- qemu-vms/freebsd/setup.sh
+++ qemu-vms/freebsd/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 usage() {
-  echo "$0 { device } [ -n ]
+  echo "$0 { device } [ -n ] [ -p poolname ] [ -a ]
 
 -n: no swap volume
+-p poolname: use specified pool name install of default zroot
+-a : autologin as root
 
 Setup a bootable ZFS filesystem on the specified device:
 - A 4G swap zvol (unless suppressed by -n)
@@ -19,16 +21,39 @@ Setup a bootable ZFS filesystem on the specified device:
 dev="$1"
 shift
 
-# Check if the -n option was passed
+# Use a simple loop to handle remaining optional args in any order
 swap=1
-[ "$1" != "-n" ] || {
+poolname=zroot
+autologin=0
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    -n)
+      swap=0
+      ;;
+
+    -p)
+      shift
+      [ "$#" -gt 0 ] || usage
+      poolname="$1"
+      ;;
+
+    -a)
+      autologin=1
+      ;;
+
+    *)
+      usage
+      ;;
+  esac
+
   shift
-  swap=0
-}
+done
 
 # Check if /boot/loader.conf contains lines for disk identification settings (not present by default)
 grep -q 'kern.geom.label' /boot/loader.conf || {
   echo 'Configuring boot delay and gptid disk identification'
+  sleep 1
 
   # Append lines to the end
   {
@@ -45,78 +70,102 @@ grep -q 'kern.geom.label' /boot/loader.conf || {
 ## Destroy any existing gpt partition on second drive
 ## The command fails if no partitions exist
 echo 'Destroying gpt partition table'
+sleep 1
 gpart destroy -F "$dev" 2> /dev/null || :
 
 ## Create an empty GPT partition table
 echo 'Creating gpt partiton table'
+sleep 1
 gpart create -s gpt "$dev"
 
 ## Create boot and zfs partitions
 echo 'Creating gpt boot and root partitions'
+sleep 1
 gpart add -t freebsd-boot -l zboot -s 512k "$dev"
-gpart add -t freebsd-zfs  -l zroot "$dev"
+gpart add -t freebsd-zfs  -l "$poolname" "$dev"
 
 ## Install boot code
 echo 'Installing gpt boot sector and gpt partition boot loader'
+sleep 1
 gpart bootcode -b /boot/pmbr -p /boot/gptzfsboot -i 1 "$dev"
 
-## Get gpt id for zroot label
+## Get gpt id for "$poolname" label
 echo 'Retrieving gpt root partition id'
-zroot_dev="/dev/gptid/`glabel status | grep "gptid.*${dev}p2" | awk '{print $1}' | awk -F/ '{print $2}'`"
+sleep 1
+zpool_dev="/dev/gptid/`glabel status | grep "gptid.*${dev}p2" | awk '{print $1}' | awk -F/ '{print $2}'`"
 
 ## Create temp mountpoint
 echo 'Creating temp mount point'
+sleep 1
 mkdir /tmp/zfs
 
 ## Create zfs pool
 echo 'Creating zpool'
-zpool labelclear -f $zroot_dev
-zpool create -m / -R /tmp/zfs zroot $zroot_dev
+sleep 1
+zpool labelclear -f $zpool_dev
+zpool create -m / -R /tmp/zfs "$poolname" $zpool_dev
 
 ## Set bootfs property
 echo 'Setting bootfs property'
-zpool set bootfs=zroot zroot
+sleep 1
+zpool set bootfs="$poolname" "$poolname"
 
 ## Install base and kernel distributions
 echo 'Entering temp mount point'
+sleep 1
 cd /tmp/zfs
 
 echo 'Installing base'
+sleep 1
 tar xvJf /usr/freebsd-dist/base.txz
 
 echo 'Installing kernel'
+sleep 1
 tar xvJf /usr/freebsd-dist/kernel.txz
 
 ## Create swap space, if desired
 [ "$swap" -eq 0 ] || {
   echo 'Creating swap space'
-  zfs create -V 4G zroot/swap
+  sleep 1
+  zfs create -V 4G "$poolname"/swap
 
   echo 'Setting freebsd swap property'
-  zfs set org.freebsd:swap=on zroot/swap
+  sleep 1
+  zfs set org.freebsd:swap=on "$poolname"/swap
 }
 
 echo 'Creating fstab'
-echo -e '#Device\t\tMountpoint\tFSType\tOptions\t\tDump\tPass\nzroot\t\t/\t\tzfs\trw,noatime\t0\t0' > etc/fstab
+sleep 1
+echo -e "#Device\t\tMountpoint\tFSType\tOptions\t\tDump\tPass\n$poolname\t\t/\t\tzfs\trw,noatime\t0\t0" > etc/fstab
 
-echo 'Creating rc.local to start DHCP for all non-loop network devices at boot, tuned for podman'
+echo -n 'Creating rc.local to start DHCP for all non-loop network devices at boot, tuned for podman'
+[ "$autologin" -eq 0 ] || echo -n 'and autologin'
+echo
+sleep 1
 cat <<-EOF > etc/rc.local
 #!/bin/sh
 for netdev in \`ifconfig -a | sed -r '/^\t/d;s,^([^:]*).*,\1,' | grep -v lo\`; do
   ifconfig \$netdev -rxcsum
   dhclient \$netdev
 done
+`[ "$autologin" -eq 0 ] || echo "/bin/sh"`
 EOF
 chmod +x etc/rc.local
 
+echo 'Setting autoboot delay to 1 second'
+sleep 1
+echo 'autoboot_delay="1"' >> boot/loader.conf
+
 echo 'Configuring boot loader to enable gptid and use zfs'
+sleep 1
 echo 'kern.geom.label.disk_ident.enable="0"' >> boot/loader.conf
 echo 'kern.geom.label.gpt.enable="0"' >> boot/loader.conf
 echo 'kern.geom.label.gptid.enable="1"' >> boot/loader.conf
 echo 'zfs_load="YES"' >> boot/loader.conf
-echo 'vfs.root.mountfrom="zfs:zroot"' >> boot/loader.conf
+echo "vfs.root.mountfrom=\"zfs:$poolname\"" >> boot/loader.conf
 
 echo 'Tuning boot loader and sysctl for podmman network performance'
+sleep 1
 echo 'hw.vtnet.X.csum_disable=1' >> boot/loader.conf
 echo 'hw.vtnet.lro_disable=1' >> boot/loader.conf
 echo 'net.link.bridge.pfil_member=0' >> etc/sysctl.conf
@@ -125,16 +174,20 @@ echo 'net.link.bridge.pfil_onlyip=0' >> etc/sysctl.conf
 
 ## Chroot and set up some stuff
 echo 'Chrooting into temp mount point'
+sleep 1
 
 cat <<-EOF | chroot .
 echo 'Configuring rc.conf'
+sleep 1
 sysrc zfs_enable="YES"
 sysrc hostname="freebsd"
 
 echo 'Setting timezone to UTC'
+sleep 1
 cp usr/share/zoneinfo/UTC etc/localtime
 
 echo 'Adjusting kernel time zone'
+sleep 1
 adjkerntz -a
 
 exit
