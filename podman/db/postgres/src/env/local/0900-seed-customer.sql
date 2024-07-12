c4c070dcd047a326b375f79c6665d6733d0c709f
Greg Hall <greghall@bantling.me>
2024-07-12 09:12:56 -0300

    Changes

--- podman/db/postgres/src/env/local/0900-seed-customer.sql
+++ podman/db/postgres/src/env/local/0900-seed-customer.sql
@@ -1,17 +1,23 @@
 -- Seed addresses
-WITH PARAMS AS (
-  SELECT 5 num_addresses
-)
-,GEN_TYPES AS (
-  SELECT (random() * (SELECT COUNT(*) FROM tables.address_type))::int typ
-        ,generate_series(1, (SELECT num_addresses FROM PARAMS))
-)
-SELECT typ
-  FROM GEN_TYPES;
+  WITH PARAMS AS (
+    SELECT 5 num_rows
+          ,(SELECT COUNT(*) FROM tables.address_type) num_types
+  )
+  ,GEN_ROWS AS (
+    SELECT *
+      FROM PARAMS
+          ,generate_series(1, (SELECT num_rows FROM PARAMS))
+  )
+  ,GEN_DATA AS (
+    SELECT (random() * (num_types - 1))::int + 1 typ
+      FROM GEN_ROWS
+  )
+  SELECT typ
+    FROM GEN_DATA;
 
 
 SELECT typ
-  FROM (SELECT (random() * (SELECT COUNT(*) FROM tables.address_type))::int typ
+  FROM (SELECT (random() * ()::int typ
       , generate_series(1, 5)) t;
 INSERT INTO tables.address(
    id
