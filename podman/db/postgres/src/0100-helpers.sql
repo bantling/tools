12df657899981ab79c29942a5c2a7072074b7a29
Greg Hall <greghall@bantling.me>
2024-11-07 09:29:16 -0400

    Changes

--- podman/db/postgres/src/0100-helpers.sql
+++ podman/db/postgres/src/0100-helpers.sql
@@ -10,7 +10,7 @@ CREATE OR REPLACE FUNCTION code.TEST(P_MSG TEXT, P_TEST BOOLEAN) RETURNS BOOLEAN
 $$
 BEGIN
   CASE
-    WHEN  P_MSG IS NULL THEN
+    WHEN P_MSG IS NULL THEN
       RAISE EXCEPTION 'P_MSG CANNOT BE NULL';
 
     WHEN NOT COALESCE(P_TEST, FALSE) THEN
@@ -180,7 +180,7 @@ BEGIN
   EXCEPTION
     WHEN OTHERS THEN
       GET STACKED DIAGNOSTICS V_MSG = MESSAGE_TEXT;
-      RAISE EXCEPTION 'code.TEST COALESCE() DIED when we provided correct error message: %', V_MSG;
+      RAISE EXCEPTION 'code.TEST COALESCE() died when we provided correct error message: %', V_MSG;
   END;
   
   -- Test error calling COALESCE(), where the error message provided IS NOT correct
@@ -192,7 +192,7 @@ BEGIN
     WHEN OTHERS THEN
       GET STACKED DIAGNOSTICS V_MSG = MESSAGE_TEXT;
       IF NOT V_MSG = 'The expected exception message ''wrong error message'' does not match the actual message ''syntax error at or near ")"''' THEN
-        RAISE EXCEPTION 'code.TEST COALESCE() did not return incorrect error message';
+        RAISE EXCEPTION 'code.TEST COALESCE() with wrong error message did not return expected error message';
       END IF;
   END;
   IF NOT V_DIED THEN
@@ -213,7 +213,7 @@ $$
 $$ LANGUAGE SQL IMMUTABLE LEAKPROOF PARALLEL SAFE;
 
 -- Test IIF
-SELECT DISTINCT code.TEST('IIF must return ' || res, code.IIF(expr, tval, fval) = res) iif
+SELECT DISTINCT code.TEST(format('code.IIF(%s, %s, %s) must return %s', expr, tval, fval, res), code.IIF(expr, tval, fval) = res) iif
   FROM (VALUES
         (TRUE,  'a'::TEXT, 'b', 'a'),
         (FALSE, 'a'::TEXT, 'b', 'b')
@@ -235,6 +235,15 @@ $$ LANGUAGE SQL IMMUTABLE LEAKPROOF PARALLEL SAFE;
 -- Test NEMPTY_WS
 SELECT DISTINCT code.TEST(msg, code.NEMPTY_WS('-', VARIADIC args) = res) nempty_ws
   FROM (VALUES
+         ('NEMPTY_WS must return a'      , ARRAY['a'                                        ], 'a'),
+         ('NEMPTY_WS must return a'      , ARRAY[NULL, 'a'                                  ], 'a'),
+         ('NEMPTY_WS must return a'      , ARRAY['a', NULL                                  ], 'a'),
+         ('NEMPTY_WS must return a'      , ARRAY[NULL, 'a', NULL                            ], 'a'),
+         ('NEMPTY_WS must return a'      , ARRAY['', 'a'                                    ], 'a'),
+         ('NEMPTY_WS must return a'      , ARRAY['a', ''                                    ], 'a'),
+         ('NEMPTY_WS must return a'      , ARRAY['', 'a', ''                                ], 'a'),
+         ('NEMPTY_WS must return a'      , ARRAY[NULL, 'a', ''                              ], 'a'),
+         ('NEMPTY_WS must return a'      , ARRAY['', 'a', NULL                              ], 'a'),
          ('NEMPTY_WS must return a-b'    , ARRAY[NULL, 'a' , '' , 'b'                       ], 'a-b'),
          ('NEMPTY_WS must return a-b-c'  , ARRAY[NULL, NULL, 'a', '', '', 'b', '', NULL, 'c'], 'a-b-c'),
          ('NEMPTY_WS must return a-b-c-d', ARRAY['a' , 'b' , 'c', 'd'                       ], 'a-b-c-d')
@@ -260,42 +269,44 @@ SELECT DISTINCT code.TEST(msg, code.IIF(ARRAY_LENGTH(ARG, 1) = 0, code.TO_8601()
 -- RELID_TO_ID converts a BIGINT to a base 62 string with a maximum of 11 chars
 -- Maximum signed BIGINT value is 9_223_372_036_854_775_807 -> AzL8n0Y58m7
 --                                                             12345678901
--- Raises an exception if P_RELID is NULL or 0, since valid relids start at 1'
+-- Raises an exception if P_RELID is NULL or < 1, since valid relids start at 1
 CREATE OR REPLACE FUNCTION code.RELID_TO_ID(P_RELID BIGINT) RETURNS VARCHAR(11) AS
 $$
 DECLARE
-  RELID BIGINT := P_RELID;
-  DIGIT CHAR;
-  ID VARCHAR(11) = '';
-  RMDR INT;
+  V_RELID BIGINT := P_RELID;
+  V_DIGIT CHAR;
+  V_ID VARCHAR(11) = '';
+  V_RMDR INT;
 BEGIN
-  IF COALESCE(RELID, 0) = 0 THEN
-    RAISE EXCEPTION 'P_RELID cannot be null or 0';
+  IF COALESCE(V_RELID, 0) < 1 THEN
+    RAISE EXCEPTION 'P_RELID cannot be NULL or < 1';
   END IF;
 
-  WHILE RELID > 0 LOOP
-    RMDR  = RELID % 62;
+  WHILE V_RELID > 0 LOOP
+    V_RMDR = V_RELID % 62;
     CASE
-      WHEN RMDR < 10      THEN DIGIT = CHR(ASCII('0') + RMDR          );
-      WHEN RMDR < 10 + 26 THEN DIGIT = CHR(ASCII('A') + RMDR - 10     );
-      ELSE                     DIGIT = CHR(ASCII('a') + RMDR - 10 - 26);
+      WHEN V_RMDR < 10      THEN V_DIGIT = CHR(ASCII('0') + V_RMDR          );
+      WHEN V_RMDR < 10 + 26 THEN V_DIGIT = CHR(ASCII('A') + V_RMDR - 10     );
+      ELSE                       V_DIGIT = CHR(ASCII('a') + V_RMDR - 10 - 26);
     END CASE;
     
     -- Add digits to the front of the string, modulus gives us the digits from least to most significant
     -- Eg for the relid 123, we get the digits 3,2,1
-    ID    = DIGIT || ID;
-    RELID = RELID /  62;
+    V_ID    = V_DIGIT || V_ID;
+    V_RELID = V_RELID /  62;
   END LOOP;
   
-  RETURN ID;
+  RETURN V_ID;
 END;
 $$ LANGUAGE plpgsql IMMUTABLE LEAKPROOF PARALLEL SAFE;
 
 --- Test RELID_TO_ID
 SELECT DISTINCT * FROM (
-  SELECT code.TEST('P_RELID cannot be null or 0', 'SELECT code.RELID_TO_ID(NULL)') relid_to_id
+  SELECT code.TEST('P_RELID cannot be NULL or < 1', 'SELECT code.RELID_TO_ID(NULL)') relid_to_id
   UNION  ALL
-  SELECT code.TEST('P_RELID cannot be null or 0', 'SELECT code.RELID_TO_ID(0)'   )
+  SELECT code.TEST('P_RELID cannot be NULL or < 1', 'SELECT code.RELID_TO_ID(0)'   )
+  UNION ALL
+  SELECT code.TEST('P_RELID cannot be NULL or < 1', 'SELECT code.RELID_TO_ID(-1)'  )
   UNION ALL
   SELECT code.TEST('RELID_TO_ID must return ' || i, code.RELID_TO_ID(r) = i)
     FROM (VALUES
@@ -320,30 +331,47 @@ SELECT DISTINCT * FROM (
 CREATE OR REPLACE FUNCTION code.ID_TO_RELID(P_ID VARCHAR(11)) RETURNS BIGINT AS
 $$
 DECLARE
+  --                                        12345678901
+  C_LPAD_ID  CONSTANT CHAR(11) := LPAD(P_ID, 11, '00000000000') COLLATE "C";
+  C_LPAD_MIN CONSTANT CHAR(11) := '00000000001'                 COLLATE "C";
+  C_LPAD_MAX CONSTANT CHAR(11) := 'AzL8n0Y58m7'                 COLLATE "C";
+  
+  C_0        CONSTANT INT := ASCII('0');
+  C_9        CONSTANT INT := ASCII('9');
+  C_CAP_A    CONSTANT INT := ASCII('A');
+  C_CAP_Z    CONSTANT INT := ASCII('Z');
+  C_LIT_A    CONSTANT INT := ASCII('a');
+  C_LIT_Z    CONSTANT INT := ASCII('z');
+  
+  C_COUNT_DIGITS           CONSTANT INT := 10;
+  C_COUNT_DIGITS_AND_LOWER CONSTANT INT := C_COUNT_DIGITS + 26; 
+  
+  V_ID          VARCHAR(11) := P_ID;
   V_DIGIT       CHAR;
   V_ASCII_DIGIT INT;
   V_RELID       BIGINT := 0;
 BEGIN
   -- P_ID cannot be null or empty
-  IF LENGTH(COALESCE(P_ID,'')) = 0 THEN
+  IF LENGTH(COALESCE(V_ID,'')) = 0 THEN
     RAISE EXCEPTION 'P_ID cannot be null or empty';
   END IF;
   
   -- P_ID must be >= '1' and <= 'AzL8n0Y58m7'. Use C collation for ASCII sorting. 
-  IF (LPAD(P_ID, 11, '00000000000') COLLATE "C" < '00000000001' COLLATE "C") OR (LPAD(P_ID, 11, '00000000000') COLLATE "C" > 'AzL8n0Y58m7' COLLATE "C") THEN
+  IF (C_LPAD_ID < C_LPAD_MIN) OR (C_LPAD_ID > C_LPAD_MAX) THEN
     RAISE EXCEPTION 'P_ID must be in the range [1 .. AzL8n0Y58m7]';
   END IF;
 
-  FOREACH V_DIGIT IN ARRAY regexp_split_to_array(P_ID, '')
+  FOREACH V_DIGIT IN ARRAY REGEXP_SPLIT_TO_ARRAY(V_ID, '')
   LOOP
     -- Get the ASCII numeric value to guarantee an ASCII comparison 
     V_ASCII_DIGIT = ASCII(V_DIGIT);
     V_RELID = V_RELID * 62;
     
     CASE
-      WHEN V_ASCII_DIGIT >= ASCII('0') AND V_ASCII_DIGIT <= ASCII('9') THEN V_RELID = V_RELID +           (V_ASCII_DIGIT - ASCII('0'));
-      WHEN V_ASCII_DIGIT >= ASCII('A') AND V_ASCII_DIGIT <= ASCII('Z') THEN V_RELID = V_RELID + 10 +      (V_ASCII_DIGIT - ASCII('A'));
-      ELSE                                                                  V_RELID = V_RELID + 10 + 26 + (V_ASCII_DIGIT - ASCII('a'));
+      WHEN V_ASCII_DIGIT >= C_0     AND V_ASCII_DIGIT <= C_9     THEN V_RELID = V_RELID +                            (V_ASCII_DIGIT - C_0);
+      WHEN V_ASCII_DIGIT >= C_CAP_A AND V_ASCII_DIGIT <= C_CAP_Z THEN V_RELID = V_RELID + C_COUNT_DIGITS +           (V_ASCII_DIGIT - C_CAP_A);
+      WHEN V_ASCII_DIGIT >= C_LIT_A AND V_ASCII_DIGIT <= C_LIT_Z THEN V_RELID = V_RELID + C_COUNT_DIGITS_AND_LOWER + (V_ASCII_DIGIT - C_LIT_A);
+      ELSE RAISE EXCEPTION 'P_ID digit ''%'' (ASCII 0x%) is invalid: only characters in the ranges of 0..9, A..Z, and a..z are valid', V_DIGIT, UPPER(TO_HEX(V_ASCII_DIGIT));
     END CASE;
   END LOOP;
   
@@ -359,9 +387,15 @@ SELECT DISTINCT * FROM (
   UNION  ALL
   SELECT code.TEST('P_ID must be in the range [1 .. AzL8n0Y58m7]', 'SELECT code.ID_TO_RELID(''0'')')
   UNION  ALL
-  SELECT code.TEST('P_ID must be in the range [1 .. AzL8n0Y58m7]', 'SELECT code.ID_TO_RELID(''-1'')')
-  UNION  ALL
   SELECT code.TEST('P_ID must be in the range [1 .. AzL8n0Y58m7]', 'SELECT code.ID_TO_RELID(''AzL8n0Y58m8'')')
+  UNION  ALL
+  SELECT code.TEST('P_ID digit ''-'' (ASCII 0x2D) is invalid: only characters in the ranges of 0..9, A..Z, and a..z are valid', 'SELECT code.ID_TO_RELID(''-1'')')
+  UNION  ALL
+  SELECT code.TEST('P_ID digit '':'' (ASCII 0x3A) is invalid: only characters in the ranges of 0..9, A..Z, and a..z are valid', 'SELECT code.ID_TO_RELID(''1:'')')
+  UNION  ALL
+  SELECT code.TEST('P_ID digit ''['' (ASCII 0x5B) is invalid: only characters in the ranges of 0..9, A..Z, and a..z are valid', 'SELECT code.ID_TO_RELID(''11['')')
+  UNION  ALL
+  SELECT code.TEST('P_ID digit ''{'' (ASCII 0x7B) is invalid: only characters in the ranges of 0..9, A..Z, and a..z are valid', 'SELECT code.ID_TO_RELID(''111{'')')
   UNION ALL
   SELECT code.TEST('ID_TO_RELID must return ' || r, code.ID_TO_RELID(i) = r)
     FROM (VALUES
